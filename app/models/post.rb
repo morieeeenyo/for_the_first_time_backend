562efe2e18b26e419062ecb9c6bbfa04c0124342
togo-mentor <uint2435@gmail.com>
2021-09-25 10:20:13 +0900

    バリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   enum genre_id: {unselected: 0, excercise: 1, nature: 2, study: 3, reading: 4, trip: 5}, _prefix: true
 
   validates :content, presence: true
-  validates :genreId, numericality: { other_than: 0 }
+  validates :user_token, presence: true
+  validates :genre_id, numericality: { other_than: 0 }
 
 end
