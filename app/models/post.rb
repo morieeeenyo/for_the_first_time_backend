2c90d8b8ceaf869e4c0f915bc1820aeaebe00546
togo-mentor <uint2435@gmail.com>
2021-10-10 08:01:12 +0900

    アソシエーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
+  belongs_to :user
 
   validates :content, presence: true
-  validates :user_token, presence: true
   validates :genre_id, numericality: { other_than: 0 }
 
 end
