6d4a31e222c00b2eb60d5350c0eeec263cfbb98d
togo-mentor <uint2435@gmail.com>
2021-10-10 08:30:14 +0900

    モジュールのinclude

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,3 @@
 class ApplicationController < ActionController::API
-  include FirebaseUtils    
-
-  private                                                        
-    # tokenが正規のものであれば、デコード結果を返す
-    # そうでなければfalseを返す
-    def authenticate_firebase_id_token
-      # authenticate_with_http_tokenは、HTTPリクエストヘッダーに
-      # Authorizationが含まれていればブロックを評価する。
-      # 含まれていなければnilを返す。
-      authenticate_with_http_token do |token, options|
-        begin
-          decoded_token = FirebaseUtils::Auth.verify_id_token(token)
-        rescue => e
-          logger.error(e.message)
-          false
-        end
-      end
-    end
+  include Firebase::Auth::Authenticable
 end
