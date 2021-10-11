46127f99377bcd8ceb3e051d523efd6354f05bfc
togo-mentor <uint2435@gmail.com>
2021-10-12 08:16:14 +0900

    コントローラー修正

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,14 @@
 class Api::UsersController < ApplicationController
 
   def create
+    FirebaseIdToken::Certificates.request
     raise ArgumentError, 'BadRequest Parameter' if payload.blank?
-    user = User.create!(sign_up_params.merge(uid: payload['sub']))
-    render json: user, serializer: AuthSerializer, status: :ok
+    user = User.find_or_initialize_by(sign_up_params.merge(uid: payload['sub']))
+    if user.save
+      render json: user, status: :ok
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
   end
 
   private
