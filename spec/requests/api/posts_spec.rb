a0d8a85b80f33a18c62cd7973a9753d9ba947f93
togo-mentor <uint2435@gmail.com>
2021-10-17 07:36:03 +0900

    rubocop

--- spec/requests/api/posts_spec.rb
+++ spec/requests/api/posts_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Api::Posts", type: :request do
-  describe "GET /api/posts" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Api::Posts', type: :request do
+  describe 'GET /api/posts' do
+    it 'works! (now write some real specs)' do
       get api_posts_index_path
       expect(response).to have_http_status(200)
     end
