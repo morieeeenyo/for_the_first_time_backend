2edb4922080c7a0877eec74b561751d548b3671b
togo-mentor <uint2435@gmail.com>
2021-10-17 07:37:47 +0900

    pending

--- spec/requests/api/posts_spec.rb
+++ spec/requests/api/posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Api::Posts', type: :request do
   describe 'GET /api/posts' do
-    it 'works! (now write some real specs)' do
+    xit 'works! (now write some real specs)' do
       get api_posts_index_path
       expect(response).to have_http_status(200)
     end
